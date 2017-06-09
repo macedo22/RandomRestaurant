37d5e7721f1afcd1b3b0c04d3b1ca1ff7459597e
gracederderian <grace.derderian@csu.fullerton.edu>
2017-06-09 16:04:36 -0700

    Create test.js

--- test.js
+++ test.js
@@ -1,23 +1,24 @@
 
-var settings = {
-  "async": true,
-  "crossDomain": true,
-  "url": "https://api.yelp.com/v3/businesses/search?location=92679",
-  "method": "GET",
-  "headers": {
-    "authorization": "Bearer 9TJvWv_jji-AYcjG2pZ8ntlJ5PLZAMRTdw78Lxeu_HfcN05yPqu74Zym9CNi6k7cYC8j52VLQm7Y2S33eFZ_lkX26PV-4-mc3wyeSPi2YTmkXgHuRn7m5cRM06M1WXYx",
-    "cache-control": "no-cache",
-    "postman-token": "4e9aa322-6810-d246-721c-799788e6c8d6"
-  },
-  "data": {
-    "client_id": "pWmZBQr8unmmV2mD6AtDRw",
-    "client_secret": "XZtHv2xIR1SJ3hv5vIMV0rT3kmShh3AYw6P9AUO8qxY9Q9exsWoBZkRuFVERCTFk",
-    "grant_type": "client_credentials"
-  }
-}
+'use strict';
+ 
+const yelp = require('yelp-fusion');
 
-$.ajax(settings).done(function (response) {
-  console.log(response);
+const clientId = 'pWmZBQr8unmmV2mD6AtDRw';
+const clientSecret = 'XZtHv2xIR1SJ3hv5vIMV0rT3kmShh3AYw6P9AUO8qxY9Q9exsWoBZkRuFVERCTFk';
+
+const token = yelp.accessToken(clientId, clientSecret).then(response => {
+  console.log(response.jsonBody.access_token);
+}).catch(e => {
+  console.log(e);
 });
 
-
+const client = yelp.client(token);
+ 
+client.search({
+  term:'Four Barrel Coffee',
+  location: 'san francisco, ca'
+}).then(response => {
+  console.log(response.jsonBody.businesses[0].name);
+}).catch(e => {
+  console.log(e);
+});
