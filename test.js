d71ef043f26d8f27f37ed3c77fba8ae6d1c8ef0f
gracederderian <grace.derderian@csu.fullerton.edu>
2017-06-09 13:26:55 -0700

    Create test.js

--- test.js
+++ test.js
@@ -1,36 +1,16 @@
-(function () {
+var settings = {
+  "async": true,
+  "crossDomain": true,
+  "url": "https://api.yelp.com/oauth2/token",
+  "method": "POST",
+  "headers": {
+    "cache-control": "no-cache",
+    "postman-token": "4e888c00-9f54-9d68-8cb9-95a31acac8b5"
+  }
+}
 
-var API_HOST = "https://api.yelp.com";
-var SEARCH_PATH = "/v3/businesses/search";
-var BUSINESS_PATH = "/v3/businesses/";
-var TOKEN_PATH = "/oauth2/token";
-var GRANT_TYPE = "client_credentials";
-
-var CLIENT_ID = "pWmZBQr8unmmV2mD6AtDRw";  // dummy id for stackoverflow purposes
-var CLIENT_SECRET = "XZtHv2xIR1SJ3hv5vIMV0rT3kmShh3AYw6P9AUO8qxY9Q9exsWoBZkRuFVERCTFk";  // dummy secret for stackoverflow purposes
-
-var SEARCH_TERM = "food";
-var SEARCH_RADIUS = 500;        // radius is in meters
-var SORT_BY = "distance";
-
-
-ajax(API_HOST + TOKEN_PATH, {
-    type: "POST",
-    data: {
-        grant_type: GRANT_TYPE,
-        client_id: CLIENT_ID,
-        client_secret: CLIENT_SECRET
-    },
-    contentType: "application/x-www-form-urlencoded",
-    dataType: "json",
-    success: function (data) {
-        console.log(data);
-
-    },
-    error: function () {
-        console.log("post call error");
-    }
-
-
-})
+$.ajax(settings).done(function (response) {
+  console.log(response);
 });
+
+
