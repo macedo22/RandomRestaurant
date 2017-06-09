5d396dee0718e7e39f2d9f5ec99ba28a025a5bf9
gracederderian <grace.derderian@csu.fullerton.edu>
2017-06-09 12:57:40 -0700

    Create test.html

--- test.js
+++
@@ -1,7 +0,0 @@
-var Yelp = require('node-yelp-fusion');
-var yelp=new Yelp({ id:pWmZBQr8unmmV2mD6AtDRw, secret:XZtHv2xIR1SJ3hv5vIMV0rT3kmShh3AYw6P9AUO8qxY9Q9exsWoBZkRuFVERCTFk});
-
-yelp.search("term=Biryani&location=New York")
-    .then(function(result){
-           res.json(result);
-        });
