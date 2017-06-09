c621e504c33a9e5b6d6738ca4d17345e134d9799
gracederderian <grace.derderian@csu.fullerton.edu>
2017-06-09 15:06:40 -0700

    Create test.js

--- test.js
+++ test.js
@@ -1,4 +1,13 @@
-var requirejs = require('requirejs');
+requirejs.config({
+    "baseUrl": "js/lib",
+    "paths": {
+      "app": "../app",
+      "jquery": "//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min"
+    }
+});
+
+// Load the main app module to start the app
+requirejs(["app/main"]);
 
 var settings = {
   "async": true,
