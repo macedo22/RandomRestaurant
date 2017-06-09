060588e512773a89cde314c4841bb68ba6161c71
gracederderian <grace.derderian@csu.fullerton.edu>
2017-06-09 13:33:16 -0700

    Create test.js

--- test.js
+++ test.js
@@ -1,4 +1,3 @@
-(function () {
 var settings = {
   "async": true,
   "crossDomain": true,
@@ -6,12 +5,16 @@ var settings = {
   "method": "POST",
   "headers": {
     "cache-control": "no-cache",
-    "postman-token": "4e888c00-9f54-9d68-8cb9-95a31acac8b5"
+    "postman-token": "b7cc69b9-5fe9-8226-8490-49a58187c8d9",
+    "content-type": "application/x-www-form-urlencoded"
+  },
+  "data": {
+    "client_id": "pWmZBQr8unmmV2mD6AtDRw",
+    "client_secret": "XZtHv2xIR1SJ3hv5vIMV0rT3kmShh3AYw6P9AUO8qxY9Q9exsWoBZkRuFVERCTFk",
+    "grant_type": "client_credentials"
   }
 }
 
 $.ajax(settings).done(function (response) {
   console.log(response);
-});
-
 });
