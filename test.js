6402c5f31560f7fd7a47eeae8ece445152e666cd
gracederderian <grace.derderian@csu.fullerton.edu>
2017-06-09 15:09:36 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,13 +1,3 @@
-requirejs.config({
-    "baseUrl": "js/lib",
-    "paths": {
-      "app": "../app",
-      "jquery": "//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min"
-    }
-});
-
-// Load the main app module to start the app
-requirejs(["app/main"]);
 
 var settings = {
   "async": true,
