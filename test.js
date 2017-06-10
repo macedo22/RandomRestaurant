eb08a30105d541762d4ade9a6e3eabd8162bfaad
gracederderian <grace.derderian@csu.fullerton.edu>
2017-06-09 19:06:57 -0700

    Create test.js

--- test.js
+++ test.js
@@ -1,15 +1,14 @@
-/****************************/
-/*Grace's test code:
+//Grace's test code:
 
 var settings = {
   "async": true,
   "crossDomain": true,
-  "url": "https://api.yelp.com/v3/businesses/search?location=92679",
-  "method": "GET",
+  "url": "https://api.yelp.com/oauth2/token",
+  "method": "POST",
   "headers": {
-    "authorization": "Bearer 9TJvWv_jji-AYcjG2pZ8ntlJ5PLZAMRTdw78Lxeu_HfcN05yPqu74Zym9CNi6k7cYC8j52VLQm7Y2S33eFZ_lkX26PV-4-mc3wyeSPi2YTmkXgHuRn7m5cRM06M1WXYx",
     "cache-control": "no-cache",
-    "postman-token": "79af742c-5be0-4d62-c30d-09cff36418ed"
+    "postman-token": "2cc616ed-3aa2-14bf-f5e0-9a1b1524da35",
+    "content-type": "application/x-www-form-urlencoded"
   },
   "data": {
     "grant_type": "client_credentials",
@@ -21,11 +20,33 @@ var settings = {
 $.ajax(settings).done(function (response) {
   console.log(response);
 });
-********************************/
+
+var settings = {
+  "async": true,
+  "crossDomain": true,
+  "url": "https://api.yelp.com/v3/businesses/search?location=92679",
+  "method": "GET",
+  "headers": {
+    "authorization": "Bearer 9TJvWv_jji-AYcjG2pZ8ntlJ5PLZAMRTdw78Lxeu_HfcN05yPqu74Zym9CNi6k7cYC8j52VLQm7Y2S33eFZ_lkX26PV-4-mc3wyeSPi2YTmkXgHuRn7m5cRM06M1WXYx",
+    "cache-control": "no-cache",
+    "postman-token": "b32abb12-cb18-b298-a1c8-a0aba03ed5ed"
+  },
+  "data": {
+    "grant_type": "client_credentials",
+    "client_id": "pWmZBQr8unmmV2mD6AtDRw",
+    "client_secret": "XZtHv2xIR1SJ3hv5vIMV0rT3kmShh3AYw6P9AUO8qxY9Q9exsWoBZkRuFVERCTFk"
+  }
+}
+
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
+
 
 /*******************************/
 /*Alex's Test Code*/
-
+/****************************/
+/*
  var settings = {
   "async": true,
   "crossDomain": true,
@@ -38,8 +59,9 @@ $.ajax(settings).done(function (response) {
   }
 };
 
-/*******************************/
+
 
 $.ajax(settings).done(function (response) {
   console.log(response);
 });
+********************************/
