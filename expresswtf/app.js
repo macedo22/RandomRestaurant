b880ffb0f4d12e14418cc3049c50233fe87e5da0
Alexandra Macedo <macedo22@ucla.edu>
2017-06-20 11:56:58 -0700

    revert syntax change

--- expresswtf/app.js
+++ expresswtf/app.js
@@ -12,8 +12,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-//app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
