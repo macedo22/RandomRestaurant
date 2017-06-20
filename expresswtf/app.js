d0e2ea4eb21b7e500440bf9bb38842e4a1acd5c5
Alexandra Macedo <macedo22@ucla.edu>
2017-06-20 11:40:16 -0700

    Change serving pulic folder syntax

--- expresswtf/app.js
+++ expresswtf/app.js
@@ -12,7 +12,8 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
