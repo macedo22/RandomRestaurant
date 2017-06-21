4826389c8fbae7f7529163882c2a7bdea6e96364
Alexandra Macedo <macedo22@ucla.edu>
2017-06-21 16:24:00 -0700

    Add functions.js as a route

--- expresswtf/app.js
+++ expresswtf/app.js
@@ -25,8 +25,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 //app.use('/', index);
-app.use('/', entry);
-app.use('/users', users);
+app.use('/', functions);
+//app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
