db4d1fd1e827484cd365550002dcbd9c67a0683a
Alexandra Macedo <macedo22@ucla.edu>
2017-06-21 15:24:36 -0700

    fix typo

--- expresswtf/app.js
+++ expresswtf/app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-var users = require('./routes/entry;);
+var users = require('./routes/entry');
 
 var app = express();
 
