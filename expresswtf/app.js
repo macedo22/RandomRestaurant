d89be7ffc161c796462971d55b59c874cea3cffa
Alexandra Macedo <macedo22@ucla.edu>
2017-06-21 15:21:32 -0700

    Route to entry as homepgae

--- expresswtf/app.js
+++ expresswtf/app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var users = require('./routes/entry;);
 
 var app = express();
 
@@ -23,7 +24,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+//app.use('/', index);
+app.use('/', entry);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
