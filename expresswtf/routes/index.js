53f9854b75abc7d611364bfce6faa786daa15587
Alexandra Macedo <macedo22@ucla.edu>
2017-06-24 22:32:50 -0700

    add Get request test response for homepage

--- expresswtf/routes/index.js
+++ expresswtf/routes/index.js
@@ -5,13 +5,14 @@ var functions_controller=require('../controllers/functions');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  //res.render('index', { title: 'Express' });
+  res.send('Received GET request WEEEEE');
 });
 
 //router.post('/', functions_controller.httpRequest);//add back after post works
 
 router.post('/', function(req,req,next){
-    res.send('Received post request WEEEEE');
+    res.send('Received POST request WEEEEE');
 });
 
 
