7244cd7b29ee820b82670610af911db293f80699
Alexandra Macedo <macedo22@ucla.edu>
2017-06-25 22:20:06 -0700

    fix html

--- expresswtf/controllers/functions.js
+++ expresswtf/controllers/functions.js
@@ -240,7 +240,7 @@ exports.httpRequest=function(req,res,next){
                     + "Food type: " + categoriesInput + "<br><br>"
                     + "First result:<br>    " + firstResult.name + "<br>"
                     + firstResult.display_phone + "<br>"
-                    + firstResult.location.address1
+                    + firstResult.location.address1 + "<br>"
                     + "<a href=firstResult.url>Find me on yelp bitches </a><br><br>"
                     + "<img src=firstResult.image_url>");
         });
