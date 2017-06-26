9f406f346671be6ae00226a0d2aabd0313a98d33
Alexandra Macedo <macedo22@ucla.edu>
2017-06-26 11:08:25 -0700

    Change back

--- expresswtf/controllers/functions.js
+++ expresswtf/controllers/functions.js
@@ -239,7 +239,7 @@ exports.httpRequest=function(req,res,next){
             var choice = matches[randomIndex];
             
             
-            res.render('index', { randomName: JSON.stringify(choice)});
+            res.render('index', { randomName: JSON.stringify(choice.name)});
             
             
            // res.send("Zipcode: " + zipInput + "<br>"
